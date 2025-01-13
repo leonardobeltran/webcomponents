c1e52db187cbe71b2f9f3935fd198f41c4295a3a
leonardobeltran <leonardo.beltran@phidias.com.co>
2025-01-13 14:13:57 -0500

    Create card.js

---
+++ card.js
@@ -0,0 +1,34 @@
+export class WCCard extends HTMLElement {
+	 constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+
+	getTemplate() {
+        const styles = `
+        <style>
+            
+        </style>
+        `;
+
+        const template = document.createElement("template");
+        template.innerHTML = `
+           <article>
+			  <header>
+			    <h1></h1>
+			    <img/>
+			  </header>
+			  <main>
+				<slot name="body"></slot>
+			  </main>
+			  <footer>
+			    <slot name="footer"></slot>
+			  </footer>
+			</article>
+            ${styles}
+        `;
+
+        return template;
+    }
+}
